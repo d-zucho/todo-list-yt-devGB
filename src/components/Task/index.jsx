08e39af59c3de2958a399d053584ed7b1553f269
D_Zucho <danny.nico.dev@outlook.com>
2022-11-21 05:09:16 -0800

    add checkbox and state functionality to task

--- src/components/Task/index.jsx
+++ src/components/Task/index.jsx
@@ -1,18 +1,30 @@
 import styles from './task.module.css'
-import circle from '../../assets/circle.png'
-import trashIcon from '../../assets/trashIcon.png'
+// import { FaRegCircle, FaRegCheckCircle } from 'react-icons/fa'
+import check from '../../assets/check.svg'
+import circle from '../../assets/circle.svg'
+import { motion, AnimatePresence } from 'framer-motion'
+import { CgTrash } from 'react-icons/cg'
+import { useState } from 'react'
 
 const Task = () => {
+  const [isCompleted, setIsCompleted] = useState(false)
+
+  const handleClick = () => {
+    setIsCompleted(!isCompleted)
+  }
+
   return (
     <div>
       <div className={styles.taskContainer}>
-        <div className='option'>
-          <img src={circle} alt='toggle' />
+        <div onClick={handleClick} className={styles.option}>
+          {/* if task is completed, show check image, if not, show empy circle */}
+          <img src={isCompleted ? check : circle} alt='completed' />
         </div>
-        <p id='todoText' className={styles.todoText}>
+        <p id='todoText' className={`${styles.todoText}`}>
           Create a new task
         </p>
-        <img className={styles.trash} src={trashIcon} alt='Delete Task' />
+
+        <CgTrash size={'1.2em'} className={styles.trash} />
       </div>
     </div>
   )
